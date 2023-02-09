fe21d00981e29aacad79f0923cedbc3aa0513294
Egor <chuvak.postal.84@mail.ru>
2023-02-10 01:26:22 +0300

    Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ира")
+{
+    Console.WriteLine("Привет, мурлыка :)");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
